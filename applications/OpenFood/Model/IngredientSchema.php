85202d17658d19bac2509e570bda1ac2dd3d8455
c9s <yoanlin93@gmail.com>
2015-04-20 17:25:20 +0800

    Use LocalizeMixinSchema

--- applications/OpenFood/Model/IngredientSchema.php
+++ applications/OpenFood/Model/IngredientSchema.php
@@ -5,9 +5,12 @@ use LazyRecord\Schema\SchemaDeclare;
 class IngredientSchema extends SchemaDeclare
 {
     public function schema() {
-        $this->column('name')->varchar(30);
+        $this->column('name')->varchar(30)->localize(['en', 'zh_TW']);
         $this->column('scientific_name')->varchar(60);
         $this->column('other_names')->varchar(60);
+
+        $this->mixin('LocalizeMixinSchema');
+        
         $this->many('foods', 'OpenFood\\Model\\FoodSchema', 'ingredient_id', 'id');
     }
 }
