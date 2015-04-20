f16def5d3a3b699507ea7b1b728782959571a352
c9s <yoanlin93@gmail.com>
2015-04-20 11:51:18 +0800

    Add basic company schema

---
+++ applications/OpenFood/Model/CompanySchema.php
@@ -0,0 +1,36 @@
+<?php
+namespace OpenFood\Model;
+use LazyRecord\Schema\SchemaDeclare;
+
+class CompanySchema extends SchemaDeclare
+{
+    public function schema() {
+
+        $this->column('name')
+            ->varchar(60)
+            ->required()
+            ;
+
+        $this->column('address')
+            ->varchar(120)
+            ;
+
+        $this->column('phone')
+            ->varchar(20)
+            ;
+
+        $this->column('fax')
+            ->varchar(20)
+            ;
+
+        $this->column('principal')
+            ->varchar(30);
+
+        $this->column('principal_phone')
+            ->varchar(20);
+
+        $this->column('locale')
+            ->validValues([ 'en', 'zh_TW' ]);
+
+    }
+}
