7a7ceeba9fb3371ea20c11c602a024c3e0e335ab
c9s <yoanlin93@gmail.com>
2015-04-20 15:42:11 +0800

    Add other_names column

--- applications/OpenFood/Model/IngredientSchema.php
+++ applications/OpenFood/Model/IngredientSchema.php
@@ -6,6 +6,8 @@ class IngredientSchema extends SchemaDeclare
 {
     public function schema() {
         $this->column('name')->varchar(30);
+        $this->column('scientific_name')->varchar(60);
+        $this->column('other_names')->varchar(60);
         $this->many('foods', 'OpenFood\\Model\\FoodSchema', 'ingredient_id', 'id');
     }
 }
